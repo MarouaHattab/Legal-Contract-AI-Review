c6da1c82f7771606c96afa052de95fc6248716a2
MarouaHattab <maroua.hattab@polytechnicien.tn>
2026-09-02 22:46:45 +0100

    complete activity 1 : extract pdf from s3

--- apps/ai-contract-review/activities.py
+++ apps/ai-contract-review/activities.py
@@ -11,37 +11,53 @@ from dotenv import load_dotenv
 from openai import OpenAI
 from temporalio import activity
 
-
-load_dotenv()
-
-#data class
-
-
-@dataclass
-class ExtractPDFInput:
-    s3_path: str
-    batch_size: int =2
-
-@dataclass
-class ExtractPDFOutput:
-    s3_path: str
-    markdown_text: str
-    page_count: int
-
-@dataclass
-class CallLLMInput:
-    prompt: str
-
-@dataclass
-class CallLLMOutput:
-    content: str
-
+from .helpers import (
+    ExtractPDFInput,
+    ExtractPDFOutput,
+    CallLLMInput,
+    CallLLMOutput,
+    get_s3_client,
+    parse_s3_path,
+)
 
 # activity 1 : extract pdf from S3
 
 @activity.defn
 async def extract_pdf(params: ExtractPDFInput) -> ExtractPDFOutput:
-    pass
+    activity.logger.info(f"Starting extraction : {params.s3_path}")
+    s3_client = get_s3_client()
+    bucket, key = parse_s3_path(params.s3_path)
+    filename = Path(key).name
+    TEMP_DIR = Path(os.environ["TEMP_DIR"])
+    local_path =str(Path(TEMP_DIR) / filename)
+
+    s3_client.download_file(bucket, key, local_path)
+
+    doc = fitz.open(local_path)
+    total_pages = doc.page_count
+    activity.logger.info(f"Downloaded {total_pages} pages from {params.s3_path}")
+    all_text_chunks = []
+    total_chars_num = 0
+
+    num_batches = math.ceil(total_pages / params.batch_size)
+
+    for batch_idx in range(num_batches):
+        start_page = batch_idx * params.batch_size
+        end_page = min(start_page + params.batch_size, total_pages)
+        batch_md= pymupdf4llm.to_markdown(
+            local_path,
+            pages = list(range(start_page, end_page)),
+        )
+        all_text_chunks.append(batch_md)
+        total_chars_num += len(batch_md)
+    full_md = "\n\n".join(all_text_chunks)
+    return ExtractPDFOutput(
+        s3_path=params.s3_path,
+        markdown_text=full_md,
+        page_count=total_pages,
+    )
+
+
 
 # activity 2 : call the llm via openrouter 
 
---
+++ apps/ai-contract-review/helpers.py
@@ -0,0 +1,43 @@
+import os
+import boto3
+from dataclasses import dataclass
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+# Dataclasses 
+@dataclass
+class ExtractPDFInput:
+    s3_path: str
+    batch_size: int = 2
+
+@dataclass
+class ExtractPDFOutput:
+    s3_path: str
+    markdown_text: str
+    page_count: int
+
+@dataclass
+class CallLLMInput:
+    prompt: str
+
+@dataclass
+class CallLLMOutput:
+    content: str
+
+#  S3 helper 
+
+def get_s3_client():
+    return boto3.client(
+        "s3",
+        aws_access_key_id=os.environ["AWS_ACCESS_KEY_ID"],
+        aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY"],
+        region_name=os.environ["AWS_REGION"],
+        endpoint_url=os.environ["AWS_S3_ENDPOINT_URL"],
+    )
+
+def parse_s3_path(s3_path: str):
+    s3_path_no_scheme = s3_path.replace("s3://", "")
+    bucket, _, key =  s3_path_no_scheme.partition("/")
+    return bucket, key
